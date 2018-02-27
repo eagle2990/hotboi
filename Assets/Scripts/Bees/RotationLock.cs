0ca16dbbba4b4de8418a29d331bc57fefe3be957
Reimo Vellemaa <reimo.vellemaa@ttu.ee>
2018-02-28 00:13:53 +0200

    enhanced enemy spawner, better folder structure

---
+++ Assets/Scripts/Bees/RotationLock.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationLock : MonoBehaviour {
+
+	// Use this for initialization
+
+	float lockPos = 0;
+
+	void Update()
+	{
+		transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, lockPos, lockPos);
+	}
+}
---
+++ Assets/Scripts/Spawner/SpawnpointChecker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnpointChecker : MonoBehaviour {
+    public bool isVisible = true;
+
+    void OnBecameVisible() {
+        isVisible = true;
+    }
+
+    void OnBecameInvisible() {
+        //print("became invisible");
+        isVisible = false;
+    }
+}
