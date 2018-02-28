6041a1f03946e255f6b094310c306893d5371c14
Reimo Vellemaa <reimo.vellemaa@ttu.ee>
2018-02-28 12:52:02 +0200

    image folder + notworking bee area damage script

---
+++ Assets/Scripts/BeeAreaDamage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeAreaDamage : MonoBehaviour {
+    public GameObject playerObject;
+    public float beeDamageRadius;
+    private Vector3 playerLocation;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerLocation = playerObject.transform.position;
+    }
+
+    void ExplosionDamage(Vector3 center, float radius) {
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+        int i = 0;
+        while (i < hitColliders.Length) {
+            hitColliders[i].SendMessage("AddDamage");
+            i++;
+        }
+    }
+}
