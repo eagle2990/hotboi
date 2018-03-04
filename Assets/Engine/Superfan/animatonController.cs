034ef14748531e2685863b32f73c692af5c976a6
Reimo Vellemaa <reimo.vellemaa@ttu.ee>
2018-03-04 14:41:38 +0200

    death animation

---
+++ Assets/Engine/Superfan/animatonController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animatonController : MonoBehaviour {
+    private Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	
+    public void Die() {
+        animator.SetBool("death", true);
+    }
+}
