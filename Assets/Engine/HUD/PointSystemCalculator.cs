29ea76c4a20c2b07ea8775790e508e9eee16c023
EAGLE <eagle2990@protonmail.com>
2018-03-04 15:35:17 +0200

    Creating PointSystemCalulator to update current score.
    Creating separate scene for developing features

---
+++ Assets/Engine/HUD/PointSystemCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointSystemCalculator : MonoBehaviour {
+
+    public FloatVariable TotalScore;
+    public FloatVariable TotalKills;
+    private RecieveDamage DamageInfo;
+    private EnemyBaseData Enemy;
+
+    private void Start()
+    {
+        DamageInfo = GetComponent<RecieveDamage>();
+        Enemy = (EnemyBaseData)DamageInfo.UnitStats;
+    }
+
+    public void AddEnemyPoints()
+    {
+        TotalKills.Add(1);
+        TotalScore.Add((Enemy.scoringPoints));
+    }
+}
