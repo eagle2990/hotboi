583e76db0a1293de5558a55bb6c86b2156d264ab
Reimo Vellemaa <reimo.vellemaa@ttu.ee>
2018-03-04 15:41:43 +0200

    animationController rename to deathController

--- Assets/Engine/Superfan/deathController.cs
+++ Assets/Engine/Superfan/deathController.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class animatonController : MonoBehaviour {
+public class deathController : MonoBehaviour {
     private Animator animator;
+    private NavMeshAgent navMesh;
+    private CapsuleCollider collider;
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
+        navMesh = GetComponent<NavMeshAgent>();
+        collider = GetComponent<CapsuleCollider>();
 	}
 	
 	// Update is called once per frame
 	
     public void Die() {
         animator.SetBool("death", true);
+        navMesh.isStopped = true;
+        collider.enabled = false;
     }
 }
