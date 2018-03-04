65da6199ede7f9dc7733bbd0bc300e2a42750e0e
Reimo Vellemaa <reimo.vellemaa@ttu.ee>
2018-03-04 17:27:20 +0200

    new scene for irma and mikk

--- Assets/Engine/HUD/flashDamage.cs
+++ Assets/Engine/HUD/flashDamage.cs
@@ -1,15 +1,38 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class flashDamage : MonoBehaviour {
     public Image damageImage;
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
-    public FloatReference flashSpeed;
+    public float flashSpeed = 5f;
+    //public float flashSpeed;
+    bool isIncrementing;
+    private float timer = 0.4f;
 
-    public void flashBlood() {
-        print("flash");
-        damageImage.color = flashColour;
-        // ... transition the colour back to clear.
-        damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
-    }
+    //public void flashBlood() {
+    //    StartCoroutine(quickFlash());
+    //}
+    
+    //IEnumerator quickFlash() {
+    //    if (isIncrementing) {
+    //        //while (timer <= 1.0f) {
+    //        //    damageImage.color = flashColour;
+    //        //    Color.Lerp(flashColour, Color.clear, timer);
+    //        //    timer += flashSpeed*Time.deltaTime;
+    //        //    yield return null;
+    //        //}
+    //        isIncrementing = false;
+
+    //    } else {
+    //        //while (timer >= 0.0f) {
+    //        //    Color.Lerp(Color.clear, flashColour, timer);
+    //        //    timer -= flashSpeed*Time.deltaTime;
+    //        //    yield return null;
+    //        //}
+    //    }
+
+
+
+    //}
 }
