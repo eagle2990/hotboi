1c95b69ecab208b76a642d4aef701304750da8e1
Reimo Vellemaa <reimo.vellemaa@ttu.ee>
2018-03-16 22:37:27 +0200

    PauseScreen, press P to pause

---
+++ Assets/Engine/HUD/Controllers/PauseScreenController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseScreenController : MonoBehaviour {
+    public GameObject pauseScreen;
+    public Button resumeButton;
+	// Use this for initialization
+	void Start () {
+        resumeButton.onClick.AddListener(ResumeGame);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P)) {
+            if (Time.timeScale != 0) {
+                PauseGame();
+            } else {
+                ResumeGame();
+            }
+
+        }
+    }
+
+    private void ResumeGame() {
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1;
+    }
+    private void PauseGame() {
+        Time.timeScale = 0;
+        pauseScreen.SetActive(true);
+    }
+}
