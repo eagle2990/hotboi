187051badef31ea8c759310373cd98b575dc5545
Reimo Vellemaa <reimo.vellemaa@ttu.ee>
2018-03-24 18:57:01 +0200

    flash damage

--- Assets/Engine/HUD/flashDamage.cs
+++ Assets/Engine/HUD/flashDamage.cs
@@ -1,38 +1,54 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class FlashDamage : MonoBehaviour {
-    public Image damageImage;
-    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
-    public float flashSpeed = 5f;
-    //public float flashSpeed;
-    bool isIncrementing;
-    private float timer = 0.4f;
+    private Image[] blinkImages;
+    public GameObject flashDamage;
+    public Color startColor;
+    public int blinkCount;
+    public float blinkDuration;
+    private Image blinkImage;
+    private bool canFlash;
 
-    //public void flashBlood() {
-    //    StartCoroutine(quickFlash());
-    //}
-    
-    //IEnumerator quickFlash() {
-    //    if (isIncrementing) {
-    //        //while (timer <= 1.0f) {
-    //        //    damageImage.color = flashColour;
-    //        //    Color.Lerp(flashColour, Color.clear, timer);
-    //        //    timer += flashSpeed*Time.deltaTime;
-    //        //    yield return null;
-    //        //}
-    //        isIncrementing = false;
+    void Start() {
+        canFlash = true;
+        blinkImages = flashDamage.GetComponentsInChildren<Image>();
+    }
 
-    //    } else {
-    //        //while (timer >= 0.0f) {
-    //        //    Color.Lerp(Color.clear, flashColour, timer);
-    //        //    timer -= flashSpeed*Time.deltaTime;
-    //        //    yield return null;
-    //        //}
-    //    }
+    public void Flash() {
+        if (canFlash) {
+            canFlash = false;
+            blinkImage = blinkImages[Random.Range(0, blinkImages.Length)];
+            //blinkImage.transform.position = new Vector3(-Random.Range(0, Screen.width), -Random.Range(0, Screen.height), 0);
+            StartCoroutine(Blink(Color.red, blinkCount, blinkDuration));
+        }
+    }
 
+    public IEnumerator ColorLerpTo(Color _color, float _duration) {
+        
+        float elapsedTime = 0.0f;
+        while (elapsedTime < _duration) {
+            blinkImage.color = Color.Lerp(blinkImage.color, _color, (elapsedTime / _duration));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
 
+    public IEnumerator Blink(Color _blinkIn, int _blinkCount, float _totalBlinkDuration) {
+        // We divide the whole duration for the ammount of blinks we will perform
+        float fractionalBlinkDuration = _totalBlinkDuration / _blinkCount;
+        for (int blinked = 0; blinked < _blinkCount; blinked++) {
+            // Each blink needs 2 lerps: we give each lerp half of the duration allocated for 1 blink
+            float halfFractionalDuration = fractionalBlinkDuration * 0.5f;
 
-    //}
+            // Lerp to the color
+            yield return StartCoroutine(ColorLerpTo(_blinkIn, halfFractionalDuration));
+
+            // Lerp to transparent
+            yield return StartCoroutine(ColorLerpTo(Color.clear, halfFractionalDuration));
+        }
+        canFlash = true;
+        
+    }
 }
