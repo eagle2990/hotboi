9dcbc906187cfe81c8e1566a693ba61363b4ffd7
EAGLE <eagle2990@protonmail.com>
2018-03-24 12:14:18 +0200

    Removing comments and unused methods

--- Assets/Engine/Sweetboi/AnimationController.cs
+++ Assets/Engine/Sweetboi/AnimationController.cs
@@ -6,24 +6,20 @@ public class AnimationController : MonoBehaviour {
     public int secondsBeforeDeathScreen;
     private Animator animator;
     private DeathScreenController deathScreenController;
-    //private CapsuleCollider collider;
     private PlayerController playerController;
     private CharacterController charController;
-	// Use this for initialization
+
 	void Start () {
         animator = gameObject.GetComponentInParent<Animator>();
         charController = gameObject.GetComponentInParent<CharacterController>();
         deathScreenController = gameObject.GetComponent<DeathScreenController>();
         playerController = gameObject.GetComponentInParent<PlayerController>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-    }
 
     public void Die() {
         StartCoroutine(DeathSequence(secondsBeforeDeathScreen));
     }
+
     IEnumerator DeathSequence(int seconds) {
         animator.SetBool("die", true);
         charController.enabled = false;
