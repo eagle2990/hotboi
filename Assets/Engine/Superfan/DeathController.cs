4604a85802283ff54dc40aa7b32fb9a6c2fbd0a3
EAGLE <eagle2990@protonmail.com>
2018-03-29 23:45:59 +0300

    Fixing bugs related to filenames uppercase and lowercase

---
+++ Assets/Engine/Superfan/DeathController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DeathController : MonoBehaviour {
+    private Animator animator;
+    private NavMeshAgent navMesh;
+    private CapsuleCollider objectCollider;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        navMesh = GetComponent<NavMeshAgent>();
+        objectCollider = GetComponent<CapsuleCollider>();
+	}
+	
+	// Update is called once per frame
+	
+    public void Die() {
+        animator.SetBool("death", true);
+        navMesh.isStopped = true;
+        objectCollider.enabled = false;
+    }
+}
