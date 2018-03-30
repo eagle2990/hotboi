c15f88ffce646a26f5dc3e6bb0b2e14032f2366d
Reimo Vellemaa <reimo.vellemaa@ttu.ee>
2018-03-30 05:04:39 +0300

    random bombing sounds between intervalls (eventsytem.prefab), created random interval event generator (eventsystem.prefab), moved bombingambient, hohe tahnen sounds to world.prefab

---
+++ Assets/Engine/Misc/RandomIntervalEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RandomIntervalEvent : MonoBehaviour {
+
+    public float leastSecondsBetweenEvents = 10f;
+    public float mostSecondsBetweenEvents = 30f;
+    private float randomTime;
+    public UnityEvent randomEvent;
+
+    private void Start(){
+        randomTime = Random.Range(leastSecondsBetweenEvents, mostSecondsBetweenEvents);
+        Invoke("RandomThing", randomTime);
+    }
+
+    private void RandomThing() {
+        randomEvent.Invoke();
+        randomTime = Random.Range(leastSecondsBetweenEvents, mostSecondsBetweenEvents);
+        Invoke("RandomThing", randomTime);
+
+     }
+}
