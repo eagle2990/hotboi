a5e92b3c8a6368069cccdf144b94692cc81d1423
Reimo Vellemaa <reimo.vellemaa@ttu.ee>
2018-04-01 21:49:49 +0300

    area kill powerup - not ready

---
+++ Assets/Engine/PowerUps/AreaKillPowerUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaKillPowerUp : MonoBehaviour {
+
+    public PlayerBaseData playerStats;
+    public float killRadius = 10f;
+    public GameObject initialExplosion;
+    public GameObject icon;
+    //public bool playerCanConsume;
+    //public bool enemiesCanConsume;
+
+    public void Appear() {
+        initialExplosion.SetActive(true);
+        icon.SetActive(true);
+    }
+
+    public void Consumed() {
+        
+    }
+
+    public void Dissapear() {
+        initialExplosion.SetActive(false);
+        icon.SetActive(false);
+    }
+}
