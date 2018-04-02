168c7c88b6b070d54d94aa4ddb333517af2297c9
Reimo Vellemaa <reimo.vellemaa@ttu.ee>
2018-04-02 18:26:20 +0300

    stepdust

---
+++ Assets/Engine/Misc/StepDust.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepDust : MonoBehaviour {
+
+    public ParticleSystem leftStepDust;
+    public ParticleSystem rightStepDust;
+    Animation animation;
+    // Use this for initialization
+    void Start() {
+    }
+
+
+    private void LeftStep() {
+        leftStepDust.Play();
+    }
+
+    private void RightStep() {
+        rightStepDust.Play();
+    }
+}
