dc5ea3662edb50c728a178ff17a1e67135d3fd08
Reimo Vellemaa <reimo.vellemaa@ttu.ee>
2018-04-05 18:23:31 +0300

    sprint powerup stays full for N-seconds and then starts depleting again

--- Assets/Engine/HUD/Sliders/UpdateSprintSlider.cs
+++ Assets/Engine/HUD/Sliders/UpdateSprintSlider.cs
@@ -10,7 +10,9 @@ public class UpdateSprintSlider : MonoBehaviour {
     public float sprintRechargeMultiplier;
     private float previousValue;
     public PlayerBaseData playerStats;
-    // Use this for initialization
+    public bool hasSprintPowerUp;
+    public float timer = 0f;
+
     void Start() {
         sprintDepletionMultiplier = 28f;
         sprintRechargeMultiplier = 10f;
@@ -19,14 +21,18 @@ public class UpdateSprintSlider : MonoBehaviour {
         slider.maxValue = playerStats.MaxSprintAmount;
     }
 
-    // Update is called once per frame
     void Update() {
         if (playerSprintAmount.Value != previousValue) {
             slider.value = playerSprintAmount.Value;
             previousValue = playerSprintAmount.Value;
         }
+
         if (Input.GetKey(KeyCode.LeftShift) && playerSprintAmount.Value > 0) {
-            playerSprintAmount.Value -= Time.deltaTime * sprintDepletionMultiplier;
+            if (timer <= 0) {
+                playerSprintAmount.Value -= Time.deltaTime * sprintDepletionMultiplier;
+            } else {
+                timer -= Time.deltaTime;
+            }
 
         }
         else if (playerSprintAmount.Value < playerStats.MaxSprintAmount && !Input.GetKey(KeyCode.LeftShift)) {
--- Assets/Engine/PowerUps/SprintPowerUp.cs
+++ Assets/Engine/PowerUps/SprintPowerUp.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class SprintPowerUp : MonoBehaviour {
     public PlayerBaseData playerStats;
-    public float sprintAddAmount = 35;
-    public GameObject initialExplotion;
-    public GameObject icon;
+    public float sprintDuration = 5f;
+    public GameObject sprintBar;
+    private UpdateSprintSlider sprintSliderScript;
+
+    private void Start() {
+        sprintSliderScript = sprintBar.transform.GetChild(0).GetComponent<UpdateSprintSlider>();
+    }
 
     public void Appear() {
-        initialExplotion.SetActive(true);
-        icon.SetActive(true);
     }
 
     public void Consumed() {
-        float addedSprintAmount = playerStats.SprintAmount.Value + sprintAddAmount;
-        playerStats.SprintAmount.SetValue(addedSprintAmount > playerStats.MaxSprintAmount.Value ? playerStats.MaxSprintAmount.Value : addedSprintAmount);
+        sprintSliderScript.timer = sprintDuration;
+        playerStats.SprintAmount.SetValue(playerStats.MaxSprintAmount.Value);
+        sprintSliderScript.hasSprintPowerUp = true;
+        //float addedSprintAmount = playerStats.SprintAmount.Value + sprintAddAmount;
+        //playerStats.SprintAmount.SetValue(addedSprintAmount > playerStats.MaxSprintAmount.Value ? playerStats.MaxSprintAmount.Value : addedSprintAmount);
     }
 
     public void Dissapear() {
-        initialExplotion.SetActive(false);
-        icon.SetActive(false);
     }
 }
