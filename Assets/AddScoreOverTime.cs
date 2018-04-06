4ff4312e3ac529b00ca7ecc1b7ab572923f897d2
Reimo Vellemaa <reimo.vellemaa@ttu.ee>
2018-04-07 02:49:07 +0300

    add score over time (add speed and score can be changed in EventSystem prefab)

---
+++ Assets/AddScoreOverTime.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddScoreOverTime : MonoBehaviour {
+
+    [Range(0, 10)]
+    public int scoreToAdd;
+    [Range(0f, 10f)]
+    public float addScoreTimer;
+    public FloatVariable score;
+
+    private float timer;
+
+    private void Start() {
+        timer = 0f;
+    }
+
+    void Update () {
+        timer += Time.deltaTime;
+        if (timer >= addScoreTimer) {
+            score.Add(scoreToAdd);
+            timer = 0f;
+        }
+	}
+
+}
