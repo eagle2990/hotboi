8e3781ca9593e9b85083a30f247d04f133a448af
wargunnerguy <reimo.vellemaa@ttu.ee>
2018-05-12 20:31:48 +0300

    enemy sinks. enemies get destroyed

--- Assets/Engine/Superfan/DeathController.cs
+++ Assets/Engine/Superfan/DeathController.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class DeathController : MonoBehaviour {
-    private Animator animator;
+    [Range(1f, 20f)]
+    private float waitTimeBeforeSink = 7f;
     public GameObject teddy;
+    
+    private Animator animator;
     private Animator burnAnimator;
     private NavMeshAgent navMesh;
     private CapsuleCollider objectCollider;
+    private new Rigidbody rigidbody;
+    private bool isDead;
 
     void Start () {
         animator = GetComponent<Animator>();
         navMesh = GetComponent<NavMeshAgent>();
         objectCollider = GetComponent<CapsuleCollider>();
+        rigidbody = GetComponent<Rigidbody>();
         if (teddy != null) {
             burnAnimator = gameObject.transform.Find("Teddy").GetComponent<Animator>();
         }
@@ -25,7 +31,16 @@ public class DeathController : MonoBehaviour {
         if (teddy != null) {
             burnAnimator.SetBool("burn", true);
         }
-        navMesh.isStopped = true;
         objectCollider.enabled = false;
+        navMesh.isStopped = true;
+        isDead = true;
+        StartCoroutine(Sink());
     }
+
+    private IEnumerator Sink() {
+        yield return new WaitForSeconds(waitTimeBeforeSink);
+        rigidbody.isKinematic = false;
+        DestroyObject(gameObject, 2f);
+    }
+
 }
