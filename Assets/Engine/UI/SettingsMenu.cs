a1d4a32493e548676fed86138a6fa82d823545d9
EAGLE <eagle2990@protonmail.com>
2018-05-16 06:03:49 -0500

    Set Volume and Brightness

--- Assets/Engine/UI/SettingsMenu.cs
+++ Assets/Engine/UI/SettingsMenu.cs
@@ -4,62 +4,68 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
-public class SettingsMenu : MonoBehaviour {
-	
-	public AudioMixer audioMixer;
-	
-	public Dropdown resolutionDropdown;
-	
-	Resolution[] resolutions;
-	
-	void Start ()
-	{
-		resolutions = Screen.resolutions;
-		
-		resolutionDropdown.ClearOptions();
-		
-		List<string> options = new List<string>();
-		
-		int currentResolutionIndex = 0;
-		for (int i = 0; i < resolutions.Length; i++)
-			
-		{
-			string option = resolutions[i].width + " x " + resolutions[i].height;
-			options.Add(option);
-			
-			if (resolutions[i].width == Screen.currentResolution.width &&
-				resolutions[i].height == Screen.currentResolution.height)
-			{
-				currentResolutionIndex = i;
-			}
-		}
-		
-		resolutionDropdown.AddOptions(options);
-		resolutionDropdown.value = currentResolutionIndex;
-		resolutionDropdown.RefreshShownValue();
-	}
+public class SettingsMenu : MonoBehaviour
+{
 
-	public void SetVolume (float volume)
-	{
-		audioMixer.SetFloat("volume", volume);
-	}
-	
-	public void SetQuality (int qualityIndex)
-	
-	{
-		QualitySettings.SetQualityLevel(qualityIndex);
-	}
-	
-	
-	public void SetFullscreen (bool isFullscreen)
-	{
-		Screen.fullScreen = isFullscreen;
-	}
+    public AudioMixer audioMixer;
+
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+    }
+
+    public void SetQuality(int qualityIndex)
+
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
 
     public void SetResolution(int resolutionIndex)
     {
         Resolution currentResolution = resolutions[resolutionIndex];
         Screen.SetResolution(currentResolution.width, currentResolution.height, Screen.fullScreen);
     }
-	
+
+    public void SetBrightness(float brightness)
+    {
+        RenderSettings.ambientLight = new Color(brightness, brightness, brightness, 1.0f);
+    }
+
 }
